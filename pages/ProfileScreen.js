010bce75cbd7d117a560165d9e7dc9292e65c56e
Christian van der Helm <79118248+1011352@users.noreply.github.com>
2023-05-22 19:00:04 +0200

    Added back-end

--- pages/ProfileScreen.js
+++ pages/ProfileScreen.js
@@ -1,30 +1,52 @@
-// React Native Bottom Navigation
-// https://aboutreact.com/react-native-bottom-navigation/
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { auth } from '../database/firebase'
 
-import * as React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+const HomeScreen = () => {
+  const navigation = useNavigation()
 
-const ProfileScreen = () => {
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 25,
-              textAlign: 'center',
-              marginBottom: 16
-            }}>
-            You are on Profile Screen
-          </Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
-export default ProfileScreen;
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+   button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
