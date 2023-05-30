bacc98961cccca2dabb4aee92acea36cdd1f233b
Dane Koenders <dane.koenders@gmail.com>
2023-05-30 13:50:36 +0200

    Update CollectionScreen with not verify items

--- pages/CollectionScreen.js
+++ pages/CollectionScreen.js
@@ -1,80 +1,96 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
-const CollectedAmount = ({ amount }) => {
-  return (
-    <View style={styles.collectedAmountContainer}>
-      <MaterialIcons name="euro" size={24} color="#ffffff" />
-      <Text style={styles.collectedAmountText}>{amount}</Text>
-    </View>
-  );
-};
-
-const Item = ({ title, price }) => {
-  return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemTitle}>{title}</Text>
-      <Text style={styles.itemPrice}>€{price}</Text>
-    </View>
-  );
-};
-
-const Page = () => {
-  const collectedAmount = 1234.56; // Replace with your actual collected amount
-  const items = [
+const CollectedPage = () => {
+  const collectedItems = [
     { title: 'Item 1', price: 10 },
     { title: 'Item 2', price: 20 },
-    { title: 'Item 3', price: 30 },
-    // Add more items as needed
+    { title: 'Item 3', price: 15 },
   ];
 
+  const notVerifiedItems = [
+    { title: 'Item 4', price: 5 },
+    { title: 'Item 5', price: 12 },
+    { title: 'Item 6', price: 8 },
+  ];
+
+  const totalCollected = collectedItems.reduce(
+    (total, item) => total + item.price,
+    0
+  );
+
   return (
-    <View style={styles.container}>
-      <CollectedAmount amount={collectedAmount} />
-      {items.map((item, index) => (
-        <Item key={index} title={item.title} price={item.price} />
-      ))}
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Total Collected: €{totalCollected}</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text style={styles.listTitle}>Collected</Text>
+          {collectedItems.map((item, index) => (
+            <View key={index} style={styles.listItem}>
+              <Text style={styles.itemTitle}>{item.title}</Text>
+              <Text style={styles.itemPrice}>€{item.price}</Text>
+            </View>
+          ))}
+        </View>
+        <View style={styles.listContainer}>
+          <Text style={styles.listTitle}>Not yet verified</Text>
+          {notVerifiedItems.map((item, index) => (
+            <View key={index} style={styles.listItem}>
+              <Text style={styles.itemTitle}>{item.title}</Text>
+              <Text style={styles.itemPrice}>€{item.price}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingTop: 40,
     backgroundColor: '#f5f5f5',
   },
-  collectedAmountContainer: {
+  header: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#2ecc71',
-    padding: 10,
     marginBottom: 20,
-    borderRadius: 8,
   },
-  collectedAmountText: {
-    color: '#ffffff',
-    marginLeft: 10,
-    fontSize: 16,
+  headerText: {
+    fontSize: 22,
     fontWeight: 'bold',
+    color: '#0782F9',
   },
-  itemContainer: {
+  listContainer: {
+    marginBottom: 20,
+  },
+  listTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  listItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
     padding: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
     marginBottom: 10,
-    borderRadius: 8,
   },
   itemTitle: {
+    flex: 1,
     fontSize: 16,
-    fontWeight: 'bold',
+    marginRight: 10,
   },
   itemPrice: {
     fontSize: 16,
+    color: '#888888',
   },
 });
 
-export default Page;
+export default CollectedPage;
