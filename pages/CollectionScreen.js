51d9027e2c2f2da43e6d194b4534855e0a237ea9
Dane Koenders <dane.koenders@gmail.com>
2023-05-30 12:40:34 +0200

    Added Collection page

---
+++ pages/CollectionScreen.js
@@ -0,0 +1,68 @@
+// React Native Bottom Navigation
+// https://aboutreact.com/react-native-bottom-navigation/
+import * as React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16
+            }}>
+            You are on Home Screen
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate(
+                'MapStack', { screen: 'Maps' }
+              )}>
+            <Text>Go to Map Tab</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate(
+                'SettingsStack', { screen: 'Settings' }
+              )}>
+            <Text>Go to Settings Tab</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Details')
+            }>
+            <Text>Open Details Screen</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+});
+export default HomeScreen;
