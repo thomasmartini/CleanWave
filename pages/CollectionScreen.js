445de14a130fec34ded763ccce1d5ce3d5ddfd80
Dane Koenders <dane.koenders@gmail.com>
2023-05-30 12:58:47 +0200

    Update CollectionScreen.js

--- pages/CollectionScreen.js
+++ pages/CollectionScreen.js
@@ -1,68 +1,80 @@
-// React Native Bottom Navigation
-// https://aboutreact.com/react-native-bottom-navigation/
-import * as React from 'react';
-import {
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Text,
-  SafeAreaView
-} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const HomeScreen = ({ navigation }) => {
+const CollectedAmount = ({ amount }) => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 25,
-              textAlign: 'center',
-              marginBottom: 16
-            }}>
-            You are on Home Screen
-          </Text>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={
-              () => navigation.navigate(
-                'MapStack', { screen: 'Maps' }
-              )}>
-            <Text>Go to Map Tab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={
-              () => navigation.navigate(
-                'SettingsStack', { screen: 'Settings' }
-              )}>
-            <Text>Go to Settings Tab</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={
-              () => navigation.navigate('Details')
-            }>
-            <Text>Open Details Screen</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
+    <View style={styles.collectedAmountContainer}>
+      <MaterialIcons name="euro" size={24} color="#ffffff" />
+      <Text style={styles.collectedAmountText}>{amount}</Text>
+    </View>
   );
-}
+};
+
+const Item = ({ title, price }) => {
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemTitle}>{title}</Text>
+      <Text style={styles.itemPrice}>€{price}</Text>
+    </View>
+  );
+};
+
+const Page = () => {
+  const collectedAmount = 1234.56; // Replace with your actual collected amount
+  const items = [
+    { title: 'Item 1', price: 10 },
+    { title: 'Item 2', price: 20 },
+    { title: 'Item 3', price: 30 },
+    // Add more items as needed
+  ];
+
+  return (
+    <View style={styles.container}>
+      <CollectedAmount amount={collectedAmount} />
+      {items.map((item, index) => (
+        <Item key={index} title={item.title} price={item.price} />
+      ))}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  collectedAmountContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+    backgroundColor: '#2ecc71',
     padding: 10,
-    width: 300,
-    marginTop: 16,
+    marginBottom: 20,
+    borderRadius: 8,
+  },
+  collectedAmountText: {
+    color: '#ffffff',
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+  itemTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  itemPrice: {
+    fontSize: 16,
   },
 });
-export default HomeScreen;
+
+export default Page;
